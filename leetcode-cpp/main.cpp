98217c3edc5cfc5509a3d9e91b5f1078d5a80ab6
Vadym Novakovskyi <vadym.novakovskyi@gmail.com>
2022-07-27 20:29:30 +0200

    Initial Commit

---
+++ leetcode-cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  leetcode-cpp
+//
+//  Created by Vadym Novakovskyi on 2022-07-27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
